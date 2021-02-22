0bd172196c879b5ba192993cf8fd4f1a45f39eef
Callum Skeet <callum@elitemail.org>
2021-02-23 09:07:15 +1100

    update layout

--- components/feed-link.js
+++ components/feed-link.js
@@ -1,13 +1,32 @@
 export default function FeedLink(props) {
   return (
-    <div className="relative py-10">
-      <a href={props.link}>
-        <div className="absolute inset-0 bg-white hover:bg-orange-400 hover:text-pink-900 hover:border-pink-900 border-2 border-black shadow-offset-purple">
-          <h3 className="font-bold font-mono text-lg relative inset-x-0 inset-y-1/3">
-            {props.text}
-          </h3>
+    <div className="group block p-4 bg-white border-2 border-black shadow-offset-purple hover:shadow-offset-orange">
+      <dl className="grid sm:block lg:grid xl:block grid-cols-1 grid-rows-3 items-center">
+        <div>
+          <dt className="sr-only">Feed name</dt>
+          <dd
+            className="font-bold font-mono text-lg"
+            aria-label={props.title}
+            aria-required="true"
+          >
+            {props.title}
+          </dd>
         </div>
-      </a>
+        <div>
+          <dd className="font-mono text-lg">
+            ◈
+            <span className="px-1">
+              <a href={props.rss}>Subscribe</a>
+            </span>
+            ◈
+          </dd>
+        </div>
+        <div>
+          <dd className="font-mono text-lg">
+            <a href={props.source}>Source</a>
+          </dd>
+        </div>
+      </dl>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,41 @@ export default function Home() {
   return (
     <div className="min-h-screen py-2 bg-yellow-100">
       <Head>
-        <title>Gov.rss</title>
+        <title>gov+rss</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main className="divide-y-8 divide-orange-200">
-        <div className="flex flex-col items-center justify-center flex-1 px-20 text-center h-80 bg-pattern">
+        <div className="flex flex-col items-center justify-center flex-1 px-20 text-center h-52 md:h-80 bg-pattern">
           <h1 className="md:text-9xl text-7xl font-bold hover:text-blue-800 bg-orange-400 hover:bg-blue-50 cursor-default">
             gov+rss
           </h1>
         </div>
-        <div className="items-center justify-center text-center px-10 md:px-40 py-10">
-          <ul>
+        <div className="items-center justify-between text-center px-10 md:px-64 lg:px-96 py-10">
+          <h2 className="text-4xl font-mono">Feeds</h2>
+          <br />
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-8">
             <li>
               <FeedLink
-                link="/feed/vic-gov.rss"
-                text="State Government of Victoria"
+                title="Victorian Premier"
+                rss="/feed/vic-prem.rss"
+                source="https://www.premier.vic.gov.au/media-centre"
+              />
+            </li>
+          </ul>
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-8">
+            <li>
+              <FeedLink
+                title="NSW Government"
+                rss="/feed/nsw-gov.rss"
+                source="https://www.nsw.gov.au/news"
+              />
+            </li>
+            <li>
+              <FeedLink
+                title="NSW Premier"
+                rss="/feed/nsw-prem.rss"
+                source="https://www.nsw.gov.au/media-releases"
               />
             </li>
           </ul>
