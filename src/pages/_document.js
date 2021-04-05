541b2ac7d61b608531fed149caa1ff08bdd6b679
Callum Skeet <callum@elitemail.org>
2021-04-05 12:20:49 +1000

    fix firefox flash of unstyled content

---
+++ src/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          {/* Firefox fix: Layout was forced before the page was fully loaded. */}
+          <script>0</script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
